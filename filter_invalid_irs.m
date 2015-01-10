52be116cb339b1088078f53326ae51609ab6bcdc
Björn Holm <bjh@kth.se>
2015-01-10 22:38:56 +0100

    filter out back ir readings

--- filter_invalid_irs.m
+++ filter_invalid_irs.m
@@ -1,5 +1,4 @@
 function ir_distances = filter_invalid_irs( ir_distances )
-
-    ir_distances(ir_distances > 0.6 | ir_distances <= 0.0) = NaN;
-
+	ir_distances(ir_distances > 0.6 | ir_distances <= 0.0) = NaN;
+	ir_distances(2,:) = NaN;
 end
